22cb1d34d47d68e688fe07c58ff09255d15dda70
kimntm <nguyenthimykim659@gmail.com>
2025-02-12 20:19:46 +0700

    cap nhap 3

--- src/pages/users/theme/header/index.js
+++ src/pages/users/theme/header/index.js
@@ -3,17 +3,20 @@ import "./style.scss"
 import { MdOutlineFacebook, MdOutlineWhereToVote } from 'react-icons/md';
 import { FaInstagram, FaRegUser, FaTwitter } from 'react-icons/fa';
 import { IoCall, IoMail } from 'react-icons/io5';
+import { AiOutlineShoppingCart } from "react-icons/ai";
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+    
     return (
+        <>
         <div className='header_top'>
             <div className='container'>
                 <div className='row'>
                     <div className='col-6 header_top_left'>
                         <ul>
                             <li><IoMail />bookshop@gmail.com</li>
-                            <li><MdOutlineWhereToVote />Đống Đa, Hà Nội</li>
+                            <li><MdOutlineWhereToVote />Đống Đa, Hà Nội, Việt Nam</li>
                         </ul>
                     </div>
                     <div className='col-6 header_top_right'>
@@ -22,12 +25,34 @@ const Header = () => {
                             <li><Link to={""}><FaInstagram /></Link></li>
                             <li><Link to={""}><FaTwitter /></Link></li>
                             <li><Link to={""}><IoCall /></Link></li>
-                            <li><Link to={""}><FaRegUser /></Link> <span>Đăng nhập</span></li>
+                            <li><Link to={""}><FaRegUser /></Link> <span>ĐĂNG NHẬP</span></li>
                         </ul>
                     </div>
                 </div>
             </div>
         </div>
+        <div className='container'>
+             <div className='row'>
+                <div className='col-xl-3'>
+                    <div className='header_logo'>
+                        <h1>BOOK SHOP</h1>
+                    </div>
+                </div>
+                <div className='col-xl-6'>MENUS</div>
+                <div className='col-xl-3'>
+                    <div className='header_cart'>
+                        <ul>
+                            <li>
+                                <Link to="">
+                                   <AiOutlineShoppingCart /> <span>0</span>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+             </div>
+        </div>
+        </>
     )
 };
 export default memo(Header);

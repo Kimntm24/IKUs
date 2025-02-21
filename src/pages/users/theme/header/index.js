be9f87365e5f699897a13339e4a949ec31f64be2
kimntm <nguyenthimykim659@gmail.com>
2025-02-21 17:12:58 +0700

    cap nhap 5

--- src/pages/users/theme/header/index.js
+++ src/pages/users/theme/header/index.js
@@ -3,12 +3,13 @@ import "./style.scss"
 import { MdOutlineFacebook, MdOutlineWhereToVote } from 'react-icons/md';
 import { FaInstagram, FaRegUser, FaTwitter } from 'react-icons/fa';
 import { IoCall, IoMail } from 'react-icons/io5';
-import { AiOutlineShoppingCart } from "react-icons/ai";
+import { AiOutlineMenu, AiOutlinePhone, AiOutlineShoppingCart } from "react-icons/ai";
 import { Link } from 'react-router-dom';
 import { ROUTERS } from 'utils/router';
 
 const Header = () => {
-    const [menus, setMenus] = useState([
+    const [isShowCategories, setShowCategories] = useState(true);
+    const [menus] = useState([
         {
             name: "Trang chủ",
             path: ROUTERS.USER.HOME,
@@ -96,6 +97,81 @@ const Header = () => {
                                     </Link>
                                 </li>
                             </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='container'>
+                <div className='row hero_categories_container'>
+                    <div className='col-lg-3 hero_categories'>
+                        <div className='hero_categories_all' onClick={() => setShowCategories(!isShowCategories)}>
+                            <AiOutlineMenu />
+                            Tất Cả Danh Mục
+                            </div>
+                        
+                            <ul className={isShowCategories ? "" : "hidden"}>
+                            <li>
+                                <Link to={"#"}>Sách Kinh Tế</Link>
+                            </li>
+                            <li>
+                                <Link to={"#"}>Sách Văn Học Trong Nước</Link>
+                            </li>
+                            <li>
+                                <Link to={"#"}>Sách Văn Học Nước Ngoài</Link>
+                            </li>
+                            <li>
+                                <Link to={"#"}>Sách Phát Triển Bản Thân</Link>
+                            </li>
+                            <li>
+                                <Link to={"#"}>Sách Tin Học Ngoại Ngữ</Link>
+                            </li>
+                            <li>
+                                <Link to={"#"}>Sách Chuyên Ngành</Link>
+                            </li>
+                            <li>
+                                <Link to={"#"}>Sách Giáo Khoa - Giáo Trình</Link>
+                            </li>
+                            <li>
+                                <Link to={"#"}>Sách Phát Hàng 2024</Link>
+                            </li>
+                            <li>
+                                <Link to={"#"}>Sách Mới 2025</Link>
+                            </li>
+                            <li>
+                                <Link to={"#"}>Review Sách</Link>
+                            </li>
+                        </ul>
+                        
+                        
+                    </div>
+                    <div className='col-lg-9 hero_search_container'>
+                        <div className='hero_search'>
+                            <div className='hero_search_form'>
+                                <form>
+                                    <input type='text' placeholder='Bạn đang tìm gì?' />
+                                    <button type='submit'>Tìm Kiếm</button>
+                                </form>
+                            </div>
+                            <div className='hero_search_phone'>
+                                <div className='hero_search_phone_icon'>
+                                    <AiOutlinePhone />
+                                </div>
+                                <div className='hero_search_phone_text'>
+                                    <p>0763.068.170</p>
+                                    <span>Hỗ trợ 24/7</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='hero_item'>
+                            {/* <div className='hero_text'>
+                                <span>Ngày Hội Siêu Sale</span>
+                                <h2>Đồng Giảm <br/> 
+                                       70%</h2>
+                                       <p>Miễn phí giao hàng tận nơi</p>
+                                       <Link to="" className='primary-btn'>
+                                       Săn Ngay
+                                       </Link>
+                            </div> */}
                         </div>
                     </div>
                 </div>
